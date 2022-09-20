b3918ca39ca09b4b87e874c7469900d928617362
Ron Almog <ron8132@gmail.com>
2022-09-20 15:28:17 -0400

    working mui v5 ts nextjs codebase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,34 @@
+import * as React from "react";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "../src/Link";
+// import ProTip from "../src/ProTip";
+// import Copyright from "../src/Copyright";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <h1>Hello...</h1>
-      <ul>
-        <li>
-          <Link href="/products">Check out our prodcts</Link>
-        </li>
-        <li>
-          <Link href="/clients">Clients</Link>
-        </li>
-        <li>
-          <Link href="/kaka">Kaka Doo</Link>
-        </li>
-      </ul>
-    </div>
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          MUI v5 + Next.js with TypeScript example
+        </Typography>
+        <Link href="/about" color="secondary">
+          Go to the about page
+        </Link>
+        {/* <ProTip />
+        <Copyright /> */}
+      </Box>
+    </Container>
   );
 };
 
